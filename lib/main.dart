05badb622aaaa087a382f2046a3f4035b74bce63
Ramazan <rdagliflutter@gmail.com>
2026-06-29 20:16:51 +0200

    Add http package and fetch raw weather data from API

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_app/secrets.dart';
 
 void main() {
   runApp(const WeatherApp());
@@ -15,9 +17,53 @@ class WeatherApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Weather App - Coming Soon'),
+      home: WeatherHomePage()
+    );
+  }
+}
+
+class WeatherHomePage extends StatefulWidget {
+  const WeatherHomePage({super.key});
+
+  @override
+  State<WeatherHomePage> createState() => _WeatherHomePageState();
+}
+
+class _WeatherHomePageState extends State<WeatherHomePage> {
+  String _result = 'Press the button to fetch weather';
+
+  Future<void> _fetchWeather() async {
+    final url = Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=Belgrade&appid=$openWeatherApiKey&units=metric');
+
+    final response = await http.get(url);
+
+    setState(() {
+      _result = response.body;
+    });
+
+    print(_result);
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Weather App')),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: _fetchWeather,
+                child: const Text('Fetch Weather'),
+              ),
+              const SizedBox(height: 20),
+              Text(_result),
+            ],
+          ),
         ),
       ),
     );
