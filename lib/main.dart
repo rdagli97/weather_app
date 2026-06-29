1a003e04b83b6d416aef3d3d4c09ec2a877d0839
Ramazan <rdagliflutter@gmail.com>
2026-06-29 17:08:32 +0200

    Initial commit: project setup

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Weather App - Coming Soon'),
+        ),
+      ),
+    );
+  }
+}
